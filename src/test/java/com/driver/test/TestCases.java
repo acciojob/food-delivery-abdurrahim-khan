05b01eca6ff9e291796e25b44eccd7fb47ed776c
abdurrahim-khan <abdurrahimalig@gmail.com>
2022-12-31 16:20:20 +0530

    aas

--- src/test/java/com/driver/test/TestCases.java
+++ src/test/java/com/driver/test/TestCases.java
@@ -2,9 +2,11 @@ package com.driver.test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@ControllerAdvice
 public class TestCases {
 
 }
