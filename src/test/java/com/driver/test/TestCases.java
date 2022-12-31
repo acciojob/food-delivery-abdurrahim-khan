eae0191383dbecfaeb33933d2f4ba77f73ac96fd
abdurrahim-khan <abdurrahimalig@gmail.com>
2022-12-31 16:05:34 +0530

    a

--- src/test/java/com/driver/test/TestCases.java
+++ src/test/java/com/driver/test/TestCases.java
@@ -6,5 +6,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class TestCases {
-
+    public static void main(String[] args) throws Exception {
+        throw new Exception();
+    }
 }
